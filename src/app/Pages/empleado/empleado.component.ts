a019ded6e0e049c3462f52ae3e336bcaf73a6365
Gustavo <animex36990@gmail.com>
2024-07-20 13:03:35 -0500

    Guardado 20/07/2024, finish

--- src/app/Pages/empleado/empleado.component.ts
+++ src/app/Pages/empleado/empleado.component.ts
@@ -5,6 +5,8 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatButtonModule } from '@angular/material/button';
 import { FormGroup, FormBuilder, ReactiveFormsModule} from '@angular/forms';
 import { EmpleadoService } from '../../Services/empleado.service';
+import { Router } from '@angular/router';
+import { Empleado } from '../../models/Empleado';
 
 @Component({
   selector: 'app-empleado',
@@ -14,8 +16,78 @@ import { EmpleadoService } from '../../Services/empleado.service';
   styleUrl: './empleado.component.css'
 })
 
-export class EmpleadoComponent /* implements OnInit */{
-/*   @Input('id') idEmpleado!: number;
+export class EmpleadoComponent implements OnInit{
+  @Input('id') idEmpleado!: number;
   private empleadoServicio = inject(EmpleadoService);
-  public formBuild = inject(FormBuilder) */
+  public formBuild = inject(FormBuilder);
+  
+  public formEmpleado: FormGroup = this.formBuild.group({
+    nombreCompleto:[''],
+    correo:[''],
+    sueldo:[0],
+    fechaContrato:['']
+  })
+
+  constructor(private router:Router){
+  }
+
+  ngOnInit(): void {
+    if(this.idEmpleado != 0){
+      this.empleadoServicio.obtener(this.idEmpleado).subscribe({
+        next:(data)=>{
+          this.formEmpleado.patchValue({
+            nombreCompleto:data.nombreCompleto,
+            correo:data.correo,
+            sueldo:data.sueldo,
+            fechaContrato:data.fechaContrato
+          })
+        },
+        error:(err)=>{
+          console.log(err.message)
+        }
+      })
+    }
+  }
+
+  guardar(){
+    const objeto: Empleado = {
+      idEmpleado: this.idEmpleado,
+      nombreCompleto: this.formEmpleado.value.nombreCompleto,
+      correo: this.formEmpleado.value.correo,
+      sueldo: this.formEmpleado.value.sueldo,
+      fechaContrato: this.formEmpleado.value.fechaContrato,
+    }
+
+    if(this.idEmpleado == 0){
+      this.empleadoServicio.crear(objeto).subscribe({
+        next:(data)=>{
+          if(data.isSuccess){
+            this.router.navigate(["/"]);
+          } else{
+            alert("Error al crear")
+          }
+        },
+        error:(err)=>{
+          console.log(err.message)
+        }
+      })
+    }else{
+      this.empleadoServicio.editar(objeto).subscribe({
+        next:(data)=>{
+          if(data.isSuccess){
+            this.router.navigate(["/"]);
+          }else{
+            alert("Error al editar")
+          }
+        },
+        error:(err)=>{
+          console.log(err.message)
+        }
+      })
+    }
+  }
+
+  volver(){
+    this.router.navigate(["/"])
+  }
 }
